284f22c71b9b009e0162cf49acb47aa6e10c49c5
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2025-01-07 12:29:45 +0100

    Learn Recursion

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Recursion;
+﻿using System.Diagnostics;
+
+namespace Recursion;
 
 class Program
 {
@@ -11,6 +13,42 @@ class Program
         LookForKey(key);
         LookForKey(innerBox);
         LookForKey(outerBox);
+        Countdown(5);
+        TestFactorial();
+        TestIsPowerOfTwo();
+
+        static void TestIsPowerOfTwo()
+        {
+            Stopwatch stopwatch = new();
+            Console.WriteLine("Son kiriting: ");
+            int i = int.Parse(Console.ReadLine()!);
+            stopwatch.Start();
+            bool result = IsPowerOfTwo(i);
+            stopwatch.Stop();
+
+            if(result)
+                Console.WriteLine($"{i} ikkini darajasi!");
+            else 
+                Console.WriteLine($"{i} ikkini darajasi emas!");
+
+            Console.WriteLine($"Vaqt : {stopwatch.ElapsedMilliseconds} millisekund");
+        }
+
+        static bool IsPowerOfTwo(int n)
+        {
+            if(n ==1 ) return true;
+            if(n < 0 || n % 2 != 0) return false;
+            
+            return IsPowerOfTwo(n / 2);
+        }
+
+        static void TestFactorial()
+        {
+            Console.WriteLine("Factorialorialni hisbolash uchun son kiriting: ");
+            int n = int.Parse(Console.ReadLine()!);
+            int result = Factorial(n);
+            Console.WriteLine($"{n}! == {result}");
+        }
 
         static void LookForKey(Item box)
         {
@@ -25,6 +63,32 @@ class Program
                     Console.WriteLine("Found the key!");
                     return;
                 }
+            }
+        }
+
+        static void Countdown(int i)
+        {
+            Console.WriteLine(i);
+
+            if(i <= 1)
+            {
+                return;
+            }
+            else
+            {
+                Countdown(i - 1);
+            }
+        }
+
+        static int Factorial(int x)
+        {
+            if(x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x -1);
             }
         }
     }
